b716fc84b7673226cfcf8d8626a3c65c37a05fb5
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-02-12 18:34:01 -0300

    feat: adiciona middleware de autenticacao

--- src/v1/routes/class.routes.ts
+++ src/v1/routes/class.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { CreateClassController } from "../controllers/classes/create-class-controller";
+import { isAutenticated } from "../middlewares/isAutenticated";
 
 const route = Router()
 
-route.post('/v1/classes', new CreateClassController().handle)
+route.post('/v1/classes', isAutenticated, new CreateClassController().handle)
 
 export { route as classesRoutes }
